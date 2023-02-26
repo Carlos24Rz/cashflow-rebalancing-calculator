8722116b6335d9b4069c84569421c04b4c8eba05
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-02-26 08:36:55 -0600

    Create portfolio rebalance algorithm and update Portfolio class implementation

--- src/classes/Portfolio.js
+++ src/classes/Portfolio.js
@@ -1,45 +1,83 @@
+/** @module classes/Porfolio */
+
+/**
+ * Represents an Investment Portfolio
+ */
 class Portfolio {
+  /**
+   * @property {Object} #assets The portfolio assets
+   */
   #assets;
 
+  /**
+   * @property {String} #assets The portfolio value
+   */
   #value;
 
+  /**
+   * @property {Function} #calculatePortolioValue Calculates the portfolio value
+   * @returns {string} The Portfolio Assets
+   */
   #calculatePortfolioValue() {
     let currentTotal = 0;
 
     Object.values(this.#assets).forEach((amount) => {
-      currentTotal += amount;
+      currentTotal += Number(amount);
     });
 
-    return currentTotal;
+    return currentTotal.toFixed(2);
   }
 
+  /**
+   * Create a portfolio
+   * @param {Object} assets The portfolio assets
+   */
   constructor(assets = {}) {
     this.#assets = assets;
     this.#value = this.#calculatePortfolioValue();
   }
 
+  /**
+   * Update the portfolio assets
+   * @param {Object} newAssets The new portolio assets
+   */
   set assets(newAssets = {}) {
     this.#assets = newAssets;
     this.#value = this.#calculatePortfolioValue();
   }
 
+  /**
+   * Get the portolio assets
+   * @return {Object} The porfolio assets
+   */
   get assets() {
     return this.#assets;
   }
 
+  /**
+   * Get the portfolio value
+   * @return {String} The porfolio value
+   */
   get value() {
     return this.#value;
   }
 
-  getAssetsPercentages() {
+  /**
+   * Get the portfolio allocation in percentages
+   * @return {Object} The porfolio allocation in percentages
+   */
+  getPortfolioAllocation() {
     const percentages = {};
 
     Object.entries(this.#assets).forEach(([assetName, assetValue]) => {
-      percentages[assetName] = assetValue / this.#value;
+      percentages[assetName] = (
+        Number(assetValue) / Number(this.#value)
+      ).toFixed(2);
     });
 
     return percentages;
   }
 }
 
+/** The portfolio class */
 export default Portfolio;
---
+++ src/utilities/rebalancePortfolio.js
@@ -0,0 +1,140 @@
+/** @module utilities/rebalancePortfolio */
+
+// An algorithm to rebalance an investment portfolio using
+// the cash-flow rebalancing technique, given the desired asset allocation
+// and the montly contribution
+import Portfolio from '../classes/Portfolio.js';
+
+/**
+ * Vanguard suggest to rebalance a portfolio only if an
+ * asset has shifted more than 5 percentage points from its target
+ * https://investor.vanguard.com/investor-resources-education/article/3-rebalancing-tips-to-fine-tune-your-portfolio
+ *
+ * @param {Portfolio} portfolio
+ * @param {Object} targetAllocation
+ */
+function isBalanced(portfolio, targetAllocation) {
+  const percentages = portfolio.getPortfolioAllocation();
+
+  for (const asset in percentages) {
+    const targetDifference = Math.abs(
+      Number(targetAllocation[asset]) - Number(percentages[asset])
+    );
+
+    if (targetDifference > 0.01) return false;
+  }
+
+  return true;
+}
+
+/**
+ * Portfolio rebalancing algorithm using recursion and a greedy approach
+ * @param {Portfolio} currentPortfolio The Current Portfolio
+ * @param {Object} targetAllocation The Desired Asset Allocation
+ * @param {Number} montlyContribution The Montly Contribution
+ * @param {Object[]} montlyInvestments The Current Monthly Investment to rebalance the portfolio
+ * @returns {Object[]} The Monthly Rebalance Investment until the portfolio is rebalanced
+ */
+function rebalance(
+  currentPortfolio,
+  targetAllocation,
+  montlyContribution,
+  montlyInvestments = []
+) {
+  //base case: Portfolio is balanced
+  if (isBalanced(currentPortfolio, targetAllocation)) {
+    console.log(currentPortfolio.assets);
+    console.log(currentPortfolio.value);
+    console.log(currentPortfolio.getPortfolioAllocation());
+
+    return montlyInvestments;
+  }
+
+  const belowTargetAssets = [];
+  const overTargetAssets = [];
+  const currentMonthInvestment = {};
+  const newAssetAllocation = { ...targetAllocation };
+
+  const portfolioAssets = currentPortfolio.assets;
+  const portfolioAllocation = currentPortfolio.getPortfolioAllocation();
+
+  Object.entries(portfolioAllocation).forEach(
+    ([assetName, assetAllocation]) => {
+      const targetDifference =
+        Number(targetAllocation[assetName]) - Number(assetAllocation);
+
+      //asset is over the target allocation
+      if (targetDifference < 0) {
+        overTargetAssets.push({
+          assetName: assetName,
+          value: Math.abs(targetDifference).toFixed(2),
+        });
+      } else if (targetDifference > 0) {
+        belowTargetAssets.push({
+          assetName: assetName,
+          value: targetDifference.toFixed(2),
+        });
+      }
+    }
+  );
+
+  //Sort by most unbalanced assets
+  belowTargetAssets.sort(
+    (currentAsset, nextAsset) =>
+      Number(nextAsset.value) - Number(currentAsset.value)
+  );
+  overTargetAssets.sort(
+    (currentAsset, nextAsset) =>
+      Number(nextAsset.value) - Number(currentAsset.value)
+  );
+
+  const rebalanceAmount = overTargetAssets.reduce(
+    (currentBalance, { assetName, value }) => {
+      const difference = Number(targetAllocation[assetName]) - Number(value);
+
+      if (difference < 0) {
+        newAssetAllocation[assetName] = '0';
+        currentBalance += Number(targetAllocation[assetName]);
+      } else {
+        newAssetAllocation[assetName] = difference.toFixed(2);
+        currentBalance += Number(value);
+      }
+
+      return currentBalance;
+    },
+    0
+  );
+
+  newAssetAllocation[belowTargetAssets[0].assetName] =
+    Number(targetAllocation[belowTargetAssets[0].assetName]) + rebalanceAmount;
+
+  Object.entries(newAssetAllocation).forEach(([assetName, assetAllocation]) => {
+    currentMonthInvestment[assetName] = (
+      Number(montlyContribution) * Number(assetAllocation)
+    ).toFixed(2);
+    portfolioAssets[assetName] = (
+      Number(portfolioAssets[assetName]) +
+      Number(currentMonthInvestment[assetName])
+    ).toFixed(2);
+  });
+
+  currentPortfolio.assets = portfolioAssets;
+  montlyInvestments.push(currentMonthInvestment);
+
+  return rebalance(
+    currentPortfolio,
+    targetAllocation,
+    montlyContribution,
+    montlyInvestments
+  );
+}
+
+function rebalancePortfolio(portfolio, targetAllocation, montlyContribution) {
+  //create a copy of the Portfolio
+  const portfolioCopy = new Portfolio(portfolio.assets);
+
+  return rebalance(portfolioCopy, targetAllocation, montlyContribution);
+}
+
+/** A Function to rebalance a portfolio */
+export default rebalancePortfolio;
